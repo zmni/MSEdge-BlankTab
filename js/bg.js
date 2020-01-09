5068e29828c18ab8233f342162a615d9bcbcff53
zmni <zmni@users.noreply.github.com>
2020-01-09 22:29:46 +0700

    formatting, comments and new set-icon size

--- js/bg.js
+++ js/bg.js
@@ -9,15 +9,15 @@ function setIcon(image) {
         path: {}
     };
 
-    for (let nr of [16,32,48,128]) {
-        data.path[nr] = `icons/${image}-${nr}.png`;
+    for (let size of [16, 32, 48, 64, 128]) {
+        data.path[size] = `icons/${image}-${size}.png`;
     }
 
     chrome.browserAction.setIcon(data);
 }
 
 function updateIcon() {
-    chrome.storage.local.get({disabled:false}, function() {
+    chrome.storage.local.get({ disabled: false }, function () {
         if (isDarkMode()) {
             setIcon('tab-light');
         } else {
@@ -26,25 +26,29 @@ function updateIcon() {
     });
 }
 
-chrome.storage.onChanged.addListener(function(changes) {
+// Monitor changes in data, and setup everything again.
+chrome.storage.onChanged.addListener(function (changes) {
     if (changes.disabled) {
         updateIcon();
     }
 });
 
-chrome.runtime.onMessage.addListener(function(request) {
+chrome.runtime.onMessage.addListener(function (request) {
     if (request.type == 'update-icon') {
         updateIcon();
     } else {
         return false;
     }
+    // This tells the browser to keep sendResponse alive because we're sending
+    // the response asynchronously.
     return true;
 });
 
+// 1st time setup
 updateIcon();
 
 chrome.runtime.onStartup.addListener(function () {
-    updateIcon();
+    updateIcon(); // Set dark/light icon...
 
     // This doesn't work yet in Chrome, but we'll put it here anyway, in case it starts working...
     let darkModeMql = window.matchMedia('(prefers-color-scheme: dark)');
