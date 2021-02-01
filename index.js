1d93de3bbb0308c65e2517b57b5d6c608675a628
zmni <zmni@users.noreply.github.com>
2021-02-02 02:45:05 +0700

    update to mv3
    - update icons
    - update screenshots
    - remove background script
    - remove permissions

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ document.title = chrome.i18n.getMessage("tabTitle");
 
 // Set favicon
 function updateFavicon(e) {
-    let color = e.matches ? "light" : "dark";
-    document.querySelector('link[rel="icon"]').href = `icons/tab-${color}.ico`;
-    // notify icon updated, only works if newtab page is open.
-    chrome.runtime.sendMessage({ message: "update-icon" });
+    const color = e.matches ? "light" : "dark";
+    document.querySelector('link[rel="icon"]').href = `icons/favicon-${color}.ico`;
 }
-const darkModeMQL = window.matchMedia("(prefers-color-scheme: dark)");
-darkModeMQL.onchange = updateFavicon;
-updateFavicon(darkModeMQL);
+const darkMode = window.matchMedia("(prefers-color-scheme: dark)");
+darkMode.addEventListener("change", updateFavicon);
+updateFavicon(darkMode);
