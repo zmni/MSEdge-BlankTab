71e4e646db221927e2527b167b60076706b987af
zmni <zmni@users.noreply.github.com>
2023-09-02 23:37:38 +0700

    Update icon

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // Set title
 document.title = chrome.i18n.getMessage("tabTitle");
-
 // Set favicon
-function updateFavicon(e) {
-    const color = e.matches ? "light" : "dark";
-    document.querySelector('link[rel="icon"]').href = `icons/favicons/${color}.ico`;
-}
-const darkMode = window.matchMedia("(prefers-color-scheme: dark)");
-darkMode.addEventListener("change", updateFavicon);
-updateFavicon(darkMode);
+//function updateFavicon(e) {
+//    const color = e.matches ? "light" : "dark";
+//    document.querySelector('link[rel="icon"]').href = `favicon-${color}.png`;
+//}
+//const darkMode = window.matchMedia("(prefers-color-scheme: dark)");
+//darkMode.addEventListener("change", updateFavicon);
+//updateFavicon(darkMode);
